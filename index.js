2116b5b7bfe22fb03d32c90ddde66c54d3dce68e
Vaughn-Alvarez <vaughn.alvarez@student.ateneo.edu>
2024-10-24 16:17:33 +0800

    fixed layout and positions, added settings

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let items = Array.from(document.getElementsByClassName("link"));
+let links = document.getElementById("links");
+links.setAttribute("data-rotate", "true");
 
 for (const key in items) {
     if (Object.prototype.hasOwnProperty.call(items, key)) {
@@ -6,7 +8,6 @@ for (const key in items) {
         element.addEventListener('mouseover', startRotation);
         element.addEventListener('mousemove', followRotation);
         // element.addEventListener('mouseout', resetRotation);
-        element.setAttribute("data-index", key)
     }
 }
 
@@ -14,6 +15,7 @@ function startRotation(event) {
     let item = event.relatedTarget;
 }
 
+let match = null;
 function followRotation(event) {
     let x = event.clientX;
     let y = event.clientY;
@@ -23,15 +25,50 @@ function followRotation(event) {
     ) {
         let itemCoords = item.getBoundingClientRect();
         let angle = Math.atan((y-(itemCoords.y+itemCoords.bottom)/2)/(x-itemCoords.x));
-        angle = angle>0.08 ? 0.08 : angle;
-        angle = angle<-0.08 ? -0.08 : angle;
-        item.style.transition = `transform 0s`;
+        angle = angle>0.084 ? 0.084 : angle;
+        angle = angle<-0.084 ? -0.084 : angle;
+        // item.style.transition = `transform 0s`;
+        // let currTransform = item.style.transform ;
+        // let currAngle = null;
+        // const regex = /-*([0-9]*\.[0-9]*)/g;
+        // match = currTransform.match(regex);
+        // console.log(match[0])                       || for some reason this does not work
         item.style.transform = `rotate(${angle}rad)`;
     }
 }
 
 function resetRotation(event) {
-    let item = event.relatedTarget;
-    // item.style.transition = `transform 2s`;
-    item.style.transform = `rotate(0rad)`;
+    for (const key in items) {
+        if (Object.prototype.hasOwnProperty.call(items, key)) {
+            const element = items[key];
+            element.style.transform = "rotate(0deg)";
+        }
+    }
+}
+
+function toggleRotation(event){
+    resetRotation(event);
+    let reset = document.getElementById("reset");
+    if (links.getAttribute("data-rotate") == "true") {
+        links.setAttribute("data-rotate", "false")
+        reset.setAttribute("disabled", "");
+        for (const key in items) {
+            if (Object.prototype.hasOwnProperty.call(items, key)) {
+                const element = items[key];
+                element.removeEventListener('mouseover', startRotation);
+                element.removeEventListener('mousemove', followRotation);
+            }
+        }
+    } else {
+        links.setAttribute("data-rotate", "true")
+        reset.removeAttribute("disabled");
+        for (const key in items) {
+            if (Object.prototype.hasOwnProperty.call(items, key)) {
+                const element = items[key];
+                element.addEventListener('mouseover', startRotation);
+                element.addEventListener('mousemove', followRotation);
+                // element.addEventListener('mouseout', resetRotation);
+            }
+        }
+    }
 }
